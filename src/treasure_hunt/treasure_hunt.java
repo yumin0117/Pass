536ece52348358edf73a43f9f36de01c3296521e
2601110285 <yuminkim9815@gmail.com>
2026-09-04 23:06:22 +0900

    2주차

---
+++ src/treasure_hunt/treasure_hunt.java
@@ -0,0 +1,11 @@
+package treasure_hunt;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class treasure_hunt {
+    static void main() {
+        Scanner s = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
