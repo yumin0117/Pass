5f70901955c1ebf8e0e0368cef19b504fcc1f4f3
2601110285 <yuminkim9815@gmail.com>
2026-09-06 19:04:00 +0900

    2주차

--- src/treasure_hunt/treasure_hunt.java
+++ src/treasure_hunt/treasure_hunt.java
@@ -21,13 +21,27 @@ public class treasure_hunt {
         int inputRow = 0;
         int inputCol = 0;
         while (true){
-            if(inputRow <= 5 && inputCol <=5){
-                System.out.println("파볼 땅의 행을 입력하세요: ");
-                inputRow = s.nextInt() - 1;
-                System.out.println("파볼 땅의 열을 입력하세요: ");
-                inputCol = s.nextInt() - 1;
-            }else {
+            System.out.println("\n--- 현재 지도 ---");
+            for (int r = 0; r < 5; r++) {
+                for (int c = 0; c < 5; c++) {
+                    if (map[r][c] == 0 || map[r][c] == 1) {
+                        System.out.print("? ");
+                    } else if (map[r][c] == 2) {
+                        System.out.print("X ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("-----------------");
+
+            System.out.println("파볼 땅의 행을 입력하세요: ");
+            inputRow = s.nextInt() - 1;
+            System.out.println("파볼 땅의 열을 입력하세요: ");
+            inputCol = s.nextInt() - 1;
+
+            if(inputRow > 4 || inputCol > 4){
                 System.out.println("잘못된 값입니다. 1부터 5까지의 숫자 중 하나만 다시 입력해주세요.");
+                continue;
             }
 
             if(map[inputRow][inputCol] == 1){
@@ -35,6 +49,7 @@ public class treasure_hunt {
                 break;
             }else if(map[inputRow][inputCol] == 0){
                 System.out.println("꽝입니다! 다시 도전해보세요!");
+                map[inputRow][inputCol] = 2;
             }else{
                 System.out.println("이미 판 땅입니다! 다시 선택해주세요!");
             }
